9887b4306ccc9202828329e6aa678721014a301f
gagansaini1212 <gagansaini12122gmail.com>
2023-11-06 19:48:58 +0530

    header burger menu working

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,28 +1,49 @@
+import { useState } from "react";
+import Link from "next/link";
 import styled from "styled-components";
 
 const Container = styled.nav`
   border-bottom: 0.5px solid #e4eaf0;
 `;
 
-const Navbar = () => (
-  <Container className="navbar" role="navigation" aria-label="main navigation">
-    <div className="navbar-brand">
-      <a className="navbar-item has-text-weight-bold" href="/">
-        ShowBox
-      </a>
-      <a
-        role="button"
-        className="navbar-burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-  </Container>
-);
+const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <Container
+      className="navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item has-text-weight-bold" href="/">
+          ShowBox
+        </a>
+        <a
+          role="button"
+          className={
+            isActive
+              ? "navbar-burger burger mobile is-active"
+              : "navbar-burger burger mobile"
+          }
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div className={isActive ? "navbar-menu is-active" : "navbar-menu"}>
+        <div className="navbar-start is-hidden-desktop">
+          <Link href="/" className="navbar-item">
+            Home
+          </Link>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 export default Navbar;
